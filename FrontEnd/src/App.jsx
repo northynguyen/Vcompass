a8f33b6fd5b5969d7442894eb73e3949f831cef8
RyoHoang <ryohoang@motorist.vn>
2024-10-05 23:47:51 +0700

    add home page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import PlaceDetails from './pages/PlaceDetails/PlaceDetails'
-import UserService from './pages/UserService/UserService'
-import Partnership from './pages/Partnership/Partnership'
-import Header from './components/Header/Header'
-import './index.css'
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 import SignIn from './components/SignIn/SignIn'
+import './index.css'
+import Partnership from './pages/Partnership/Partnership'
+import PlaceDetails from './pages/PlaceDetails/PlaceDetails'
+import UserService from './pages/UserService/UserService'
 
 function App() {
   const [showLogin, setShowLogin] = useState(false)
