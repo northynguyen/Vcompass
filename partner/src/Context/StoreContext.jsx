c0e7cfae006a06022ebf81b9d3de64790d90d82c
Pham Ba Thanh <93045641+PThanh1803@users.noreply.github.com>
2024-12-12 21:29:48 +0700

    Update StoreContext.jsx

--- partner/src/Context/StoreContext.jsx
+++ partner/src/Context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
   const [token, setToken] = useState("")
   const [user, setUser] = useState({})
-  const url = "http://localhost:4000"
+  const url = "https://vcompass-backend.onrender.com"
+
 
   const fetchpartner = async (authtoken) => {
     if (authtoken) {
