ede7cc4cc58753ee31d3c25dc930db70fc0dd309
RyoHoang <121418786+ThienGB@users.noreply.github.com>
2025-05-30 18:31:02 +0700

    Update StoreContext.jsx

--- admin/src/Context/StoreContext.jsx
+++ admin/src/Context/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
   const [token, setToken] = useState("")
   const [admin, setAdmin] = useState({})
-  const url = "http://localhost:4000"
+  const url = "https://vcompass-backend.onrender.com"
+ // const url = "http://localhost:4000"
 
 
   const fetchAdmin = async (authtoken) => {
